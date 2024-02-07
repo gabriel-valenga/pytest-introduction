239513fff6fab67f0705e111413328a0dd746bf4
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2024-02-07 07:59:35 -0300

    Created first test case file. Added gitignore to project.

---
+++ TC_001_First_TestCase.py
@@ -0,0 +1,6 @@
+#Test case vode must be inside a method
+#Test case method must be started with 'test_' 
+
+def test_tc_001_Login_Logout_Testing():
+    print('This is our test case code')
+    print('This is end of our test case code')
