5695e9cb1aae6a4c400105da43b02c8f87d5f388
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2024-02-07 08:35:14 -0300

    Added one more test case to a file. Added annotation about verbose commands and output display on console.

--- AutomationCases/test_TC_001_First_TestCase.py
+++ AutomationCases/test_TC_001_First_TestCase.py
@@ -4,3 +4,12 @@
 def test_tc_001_Login_Logout_Testing():
     print('This is our test case code')
     print('This is end of our test case code')
+
+
+def test_tc_003_Login_Logout_Invalid_Credentials():
+    print('This is my testcase 3')
+    print('This is end of testcase')
+
+
+# Print statement output display on console: pytest -s 
+# Verbose mode - display test cases name with status: pytest -v     
