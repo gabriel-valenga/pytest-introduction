238f5de1c478ba8d97f45a8bf96ed64790e34173
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2024-02-07 08:17:40 -0300

    Created a folder to place the tests. Created a new file test_002

---
+++ AutomationCases/test_TC_002_Second_TestCase.py
@@ -0,0 +1,6 @@
+#Test case vode must be inside a method
+#Test case method must be started with 'test_' 
+
+def test_tc_002_Registration_Testing():
+    print('This is our 2nd test case code')
+    print('This is end of our test case code')
