c3ecd49ab4e28a8250210f745471ffa6f63ff034
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2024-02-12 19:35:26 -0300

    Added decorator to skip the selected test

--- AutomationCases/test_TC_001_First_TestCase.py
+++ AutomationCases/test_TC_001_First_TestCase.py
@@ -1,6 +1,11 @@
+import pytest
 #Test case vode must be inside a method
 #Test case method must be started with 'test_' 
 
+a=101
+
+#Decorator
+@pytest.mark.skipif(a>100, reason="Skip as this functionality is not working, developer will fix it in new build")
 def test_tc_001_Login_Logout_Testing():
     print('This is our test case code')
     print('This is end of our test case code')
