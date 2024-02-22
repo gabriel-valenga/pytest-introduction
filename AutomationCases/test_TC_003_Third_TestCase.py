b594e191822f1b4d28f8e6e3884b3a47aa59f610
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2024-02-22 08:36:39 -0300

    Created file with fixture code examples

--- AutomationCases/test_TC_003_Third_TestCase.py
+++ AutomationCases/test_TC_003_Third_TestCase.py
@@ -2,16 +2,23 @@ import pytest
 #Test case vode must be inside a method
 #Test case method must be started with 'test_' 
 
+# @pytest.fixture(scope='module') #to execute fixture code only before the first test case and after the last test case in this file
+@pytest.fixture()
+def fixture_code():
+    print('This is our fixture code, will be executed before the test case')
+    yield 
+    print('This is our fixture code, will be executed after the test case')
+
 #Decorator
 @pytest.mark.Smoke
 @pytest.mark.Regression
-def test_tc_001_Login_Logout_Testing():
+def test_tc_001_Login_Logout_Testing(fixture_code):
     print('This is Smoke')
     print('This is end of our test case code')
 
 @pytest.mark.Sanity
 @pytest.mark.Regression
-def test_tc_003_Login_Logout_Invalid_Credentials():
+def test_tc_003_Login_Logout_Invalid_Credentials(fixture_code):
     print('This is Sanity')
     print('This is end of testcase')
 
