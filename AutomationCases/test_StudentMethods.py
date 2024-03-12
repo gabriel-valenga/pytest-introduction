4b324a5abdfd5ecdd0171cc5baeda955eaf0e4b6
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2024-03-12 08:49:23 -0300

    Renamed file test_AddNewStudent to test_StudentMethods; Created get student test method

---
+++ AutomationCases/test_StudentMethods.py
@@ -0,0 +1,18 @@
+import requests
+import json
+import jsonpath 
+
+def test_add_student_data():
+    API_URL="http://thetestingworldapi.com/api/studentsDetails"
+    with open('AddNewStudent.json', 'r') as file:
+        json_request = file.read()
+    response = requests.post(API_URL, json_request)
+    print(response)
+
+
+def test_get_student_data():
+    API_URL="http://thetestingworldapi.com/api/studentsDetails/3034"
+    response = requests.get(API_URL)
+    json_response = json.loads(response.text)
+    id = jsonpath.jsonpath(json_response,'data.id')
+    assert id[0] == 3034
