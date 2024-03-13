9d761e9e6b4cd69547746aae719724e640005c1f
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2024-03-13 08:11:18 -0300

    Created methods to update and delete student

--- AutomationCases/test_StudentMethods.py
+++ AutomationCases/test_StudentMethods.py
@@ -16,3 +16,17 @@ def test_get_student_data():
     json_response = json.loads(response.text)
     id = jsonpath.jsonpath(json_response,'data.id')
     assert id[0] == 3034
+
+
+def test_update_student_data():
+    API_URL="http://thetestingworldapi.com/api/studentsDetails/3034"
+    with open('UpdateStudent.json', 'r') as file:
+        json_request = file.read()
+    response = requests.put(API_URL, json_request)
+    print(response.text)
+
+
+def test_delete_student():
+    API_URL="http://thetestingworldapi.com/api/studentsDetails/3034"
+    response = requests.delete(API_URL)
+    print(response.text)
