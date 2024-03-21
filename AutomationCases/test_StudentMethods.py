f72fc52e13eb7c127610b11a8335eebd33fcecc1
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2024-03-20 21:30:37 -0300

    Created methods to use in request chaining

--- AutomationCases/test_StudentMethods.py
+++ AutomationCases/test_StudentMethods.py
@@ -1,32 +1,73 @@
 import requests
 import json
 import jsonpath 
+from http import HTTPStatus
+
+student_id = ''
 
 def test_add_student_data():
+    global student_id
     API_URL="http://thetestingworldapi.com/api/studentsDetails"
-    with open('AddNewStudent.json', 'r') as file:
+    with open(r'AddNewStudent.json', 'r') as file:
         json_request = file.read()
     response = requests.post(API_URL, json_request)
-    print(response)
+    student = json.loads(response.text)
+    student_id = jsonpath.jsonpath(student[0], 'id')[0]
+    print(student_id)
+    assert response.status_code == HTTPStatus.CREATED
+
 
 
 def test_get_student_data():
-    API_URL="http://thetestingworldapi.com/api/studentsDetails/3034"
+    global student_id
+    API_URL= f"http://thetestingworldapi.com/api/studentsDetails/{student_id}"
     response = requests.get(API_URL)
     json_response = json.loads(response.text)
-    id = jsonpath.jsonpath(json_response,'data.id')
-    assert id[0] == 3034
+    id = jsonpath.jsonpath(json_response,'data.id')[0]
+    assert response.status_code == HTTPStatus.OK
 
 
 def test_update_student_data():
-    API_URL="http://thetestingworldapi.com/api/studentsDetails/3034"
-    with open('UpdateStudent.json', 'r') as file:
+    global student_id
+    API_URL=f"http://thetestingworldapi.com/api/studentsDetails/{student_id}"
+    with open(r'UpdateStudent.json', 'r') as file:
         json_request = file.read()
     response = requests.put(API_URL, json_request)
-    print(response.text)
+    assert response.status_code == HTTPStatus.OK
 
 
 def test_delete_student():
-    API_URL="http://thetestingworldapi.com/api/studentsDetails/3034"
+    global student_id
+    API_URL=f"http://thetestingworldapi.com/api/studentsDetails/{student_id}"
     response = requests.delete(API_URL)
+    assert response.status_code == HTTPStatus.NO_CONTENT
+
+
+def test_add_student_tech_details():
+    global student_id
+    API_URL = f"http://thetestingworldapi.com/api/technicalskills/{student_id}"
+    with open(r'StudentTechDetailsData.json', 'r') as file:
+        request_json = json.loads(file.read())
+    response = requests.put(API_URL, request_json)
     print(response.text)
+    assert response.status_code == HTTPStatus.OK
+
+    
+def test_add_student_addresses():
+    global student_id
+    API_URL = f"http://thetestingworldapi.com/api/adresses/{student_id}"
+    with open(r'StudentAdresses.json', 'r') as file:
+        request_json = json.loads(file.read())
+    response = requests.post(API_URL, request_json)
+    print(response.text)
+    assert response.status_code == HTTPStatus.OK
+
+
+def test_get_student_final_details():
+    global student_id
+    API_URL = f"http://thetestingworldapi.com/api/FinalStudentDetails/{student_id}"
+    response = requests.get(API_URL)
+    print(response.text)
+    assert response.status_code == HTTPStatus.OK
+
+
