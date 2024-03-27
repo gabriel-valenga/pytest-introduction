3c83d2e676577ec26401d08f4c16636d767a8ae4
Gabriel Valenga <gabrielvalengalemos@yahoo.com>
2024-03-27 08:44:23 -0300

    Created methods to fetch sheet key names and format json request

--- DataDriven/Library.py
+++ DataDriven/Library.py
@@ -10,9 +10,28 @@ class ExcelSheet:
         workbook = load_workbook(fileNamePath)
         sheet = workbook[SheetName]
 
+
     def fetch_sheet_row_count(self):
         return sheet.max_row
     
 
     def fetch_sheet_column_count(self):
         return sheet.max_column
+
+
+    def fetch_sheet_key_names(self):
+        sheet_max_columns = self.fetch_sheet_column_count()
+        key_names= []
+        for i in range(i, sheet_max_columns + 1):
+            cell = sheet.cell(row=1, column=i)
+            key_names.insert(cell.value)
+        return key_names
+    
+
+    def format_json_request(self, row_number):
+        json_request = {}
+        sheet_max_columns = self.fetch_sheet_column_count()
+        key_names = self.fetch_sheet_key_names()
+        for i in range(1, sheet_max_columns + 1):
+            cell = sheet.cell(row=row_number, column=i)
+            json_request.update({key_names[i-1]: cell.value})
